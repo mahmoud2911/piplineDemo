fcdf7e3a56bb637d16440828448ffe0665c06edb
mahmoud2911 <mahmoud.ahmed@foodics.com>
2023-12-07 15:27:56 +0200

    Publish Allure report to GitHub Pages

--- generate_allure_report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "/Users/hussainhakim/.m2/repository/allure/allure-2.17.3/bin/"
-bash allure serve "$parent_path/allure-results"
-exit
